d31bc839585ed0747e62b665104dff3cbc2085c9
Matt Renaud <mrenaud92@gmail.com>
2016-11-03 22:15:44 -0700

    Add deployment script.

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+deploy_directory=dist
+deploy_branch=gh-pages
+
+previous_branch=`git rev-parse --abbrev-ref HEAD`
+commit_hash=`git log -n 1 --format="%H" HEAD`
+
+# Switch to deployment branch.
+git checkout ${deploy_branch}
+
+# Copy generated files to root directory.
+rsync -av dist/* .
+
+# Add and commit changes.
+git add --all
+git commit -m "Deploying from ${commit_hash}"
+
+# Switch back to previous branch.
+git checkout ${previous_branch}
